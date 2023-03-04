3c5b45d6985049453fe3bb1777ac33193a92a58c
bmay <bloodymay456@gmail.com/ssh>
2023-03-04 17:03:58 +0300

    tests added

---
+++ venv/tests.py
@@ -0,0 +1,5 @@
+#from HW3_task20 import "function name"
+#from timeit import timeit
+#print (timeit("function_name()", globals=globals()))
+#or
+#print(timeit("function_name()", setup="from HW3_task20 import "function name"")
