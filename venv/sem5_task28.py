cea9a0884fce4ed6dd2b29a6f36eaf2d07717242
bmay <bloodymay456@gmail.com/ssh>
2023-03-06 16:36:32 +0300

    s5_task28(fixed) added

--- venv/sem5_task28.py
+++ venv/sem5_task28.py
@@ -9,15 +9,15 @@
 """
 from timeit import timeit
 
-a = int(input('Введите число a: '))
-b = int(input('Введите число b: '))
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
 if a >= 0 and b >= 0:
     def sum_function(a, b):
 
         if a == 0:
             return b
-        return sum_function(a-1, b+1)
-    print(timeit("sum_function(a, b)", globals=globals()))
+        return sum_function(a - 1, b + 1)
+    print(timeit('sum_function(a, b)', globals=globals()))
     print(f"Сумма чисел '{a}' и '{b}' равно: {sum_function(a, b)}")
 
 else: print("Вы ввели отрицательное число. Введите другое. ")
