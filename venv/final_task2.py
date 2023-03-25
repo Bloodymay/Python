8bc37fb590af3069dd4781d2c9a245a53c94e907
bmay <bloodymay456@gmail.com/ssh>
2023-03-25 21:56:17 +0300

    final_task2 fixed

--- venv/final_task2.py
+++ venv/final_task2.py
@@ -10,19 +10,12 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
-my_list = ['class', 'function', 'method']
-my_bytes_list = [b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0',
-                 b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82',
-                 b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80']
+my_list = [b'class', b'function', b'method']
 for i in my_list:
     print(f'Тип переменной: {type(i)}')
     print(f'Содержимое переменной: {i}')
     print(f'Длина переменной: {len(i)}')
 
-for i in my_bytes_list:
-    print(f'Тип переменной: {type(i)}')
-    print(f'Содержимое переменной: {i}')
-    print(f'Длина переменной: {len(i)}')
 
 
 
